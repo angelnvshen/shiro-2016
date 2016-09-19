8c137f9f896bbaf9cf6454ae8a509ec28e5eb02e
angelnvshen <angelnvshen@163.com>
2016-09-19 17:11:58 +0800

    shiro web test

---
+++ src/main/java/own/stu/shiro/servlet/AdminServlet.java
@@ -0,0 +1,26 @@
+package own.stu.shiro.servlet;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by dell on 2016/9/12.
+ */
+public class AdminServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Admin get ..... ");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Admin post ..... ");
+    }
+}
---
+++ src/main/java/own/stu/shiro/servlet/LoginServlet.java
@@ -0,0 +1,41 @@
+package own.stu.shiro.servlet;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by dell on 2016/9/12.
+ */
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("login get ..... ");
+        req.getRequestDispatcher("login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("login post ..... ");
+        String userName = req.getParameter("userName");
+        String password = req.getParameter("password");
+
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(userName, password);
+        try {
+            subject.login(token);
+            resp.sendRedirect("success.jsp"); //登录成功 跳转
+        }catch (Exception e){
+            e.printStackTrace();
+            req.setAttribute("errorInfo", "用户名或密码错误");
+            req.getRequestDispatcher("login.jsp").forward(req, resp);
+        }
+
+    }
+}
